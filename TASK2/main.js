5925d4081c62e36a138c9753f94361b1dce92b43
Dmytro-L <Lishnovskiy.dima@gmail.com>
2020-09-22 17:07:44 +0300

    last commitЕ

--- TASK2/main.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    const expandBtn = document.getElementById('expandBtn')
-    const wrap = document.getElementById('wrap')
-
-    expandBtn.onclick = () => {
-        wrap.classList.toggle("expanded")
-        expandBtn.innerHTML = wrap.classList.contains('expanded') ? '&#129125 SHRINK' : '&#129125 EXPAND'
-    }
-})()
